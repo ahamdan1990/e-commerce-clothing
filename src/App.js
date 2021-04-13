f6d610069a993ffb611ecb5e32db74ec52b1d200
Ali Hamdan <ashamdan1990@gmail.com>
2021-04-13 20:15:47 +0300

    update our menu-items to navigate withRouter HOC:

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
 import { HomePage } from './pages/homepage/homepage.component';
+import { Switch,Route,Link } from 'react-router-dom';
+
+const HatsPage = (props) => (
+  <div>
+    <h1>HATS PAGE</h1>
+    {/* <Link to="/">Home</Link>
+    <button onClick={()=>props.history.push('/')}>Home</button> */}
+  </div>
+)
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      {/* <HomePage /> */}
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './homepage.styles.scss';
 import Directory from '../../components/directory/directory.component';
+import {Link} from 'react-router-dom';
 
 export const HomePage = () => (
     <div className='homepage'>
         <Directory />
+        {/* <Link to='/hats'>Hat</Link> */}
     </div>
 )
 
