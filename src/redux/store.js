aec810672a9e135e234cb79b6dbbd35ca5a9812e
Ali Hamdan <ashamdan1990@gmail.com>
2021-04-25 18:52:07 +0300

    ''

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,7 +5,11 @@ import { persistStore } from 'redux-persist';
 import rootReducer from './root-reducer';
 
 //Set up a middlwares constant that will hold an array of all middlwares we want to apply to our application
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === 'development') {
+    middlewares.push(logger)
+}
 
 //setup the store by calling the createstore functions which take the rootReducer and applyMiddleware function which we pass into it all the middlewares we want to use by spreading them as param for this function
 export const store = createStore(rootReducer,applyMiddleware(...middlewares));
